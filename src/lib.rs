ce594f19aa8d8320d26628914871689bddbf4a58
t-eckert <t_eckert@outlook.com>
2018-08-05 11:42:04 -0700

    created test framework

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+#[cfg(test)]
+mod tests {
+  	#[test]
+	fn test_basic() {
+		assert!(1 == 1);
+	}
+
+  	// fn test_create_player() {
+    // 	// arrange
+		// let expected_name = String::from("Thomas");
+		// let expected_points = 0;
+
+		// // act
+		// test_player = Player::new("Thomas");
+
+		// // assert
+		// assert_eq!(expected_name, test_player.name);
+		// assert_eq!(expected_points, test_player.points);
+  	
+}
