b1e0050bf8ef81ab1ec0dae699a1703addd738f5
t-eckert <t_eckert@outlook.com>
2018-08-09 20:39:57 -0700

    moved spinWheel to wheel.rs

--- src/game/wheel.rs
+++ src/game/wheel.rs
@@ -1,3 +1,6 @@
+extern crate rand;
+use rand::Rng;
+
 pub struct Wheel {
   
 }
@@ -8,4 +11,12 @@ impl Wheel {
 
     }
   }
+}
+
+fn spin_wheel() -> i32 {
+    let mut rng = rand::thread_rng();
+    let wheel = vec![2500, 600, 700, 600, 650, 500, 700, 600, 550, 500, 600, 650, 700, 800, 500, 650, 500, 900];
+    let index = rng.gen_range(0, wheel.len());
+
+    wheel[index]
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-extern crate rand;
-use rand::Rng;
-use std::io;
-// use std::collections;
-
-
 #[cfg(test)]
 mod main_tests;
 
@@ -20,17 +14,3 @@ fn main() {
         round += 1;
     }
 }
-
-
-fn spin_wheel() -> i32 {
-    let mut rng = rand::thread_rng();
-    let wheel = vec![2500, 600, 700, 600, 650, 500, 700, 600, 550, 500, 600, 650, 700, 800, 500, 650, 500, 900];
-    let index = rng.gen_range(0, wheel.len());
-
-    wheel[index]
-}
-
-// fn spinWheel -> integer value of pts. (eventually a lose a turn result)
-
-// fn getPhrase() returns a phrase from a JSON file
-
