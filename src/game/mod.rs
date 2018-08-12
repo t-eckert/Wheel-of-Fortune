8b89322b5d435f1a7be6cf781a3644bebddc3393
t-eckert <t_eckert@outlook.com>
2018-08-12 14:50:19 -0700

    wheel as array, not vector.

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -14,7 +14,7 @@ use self::rand::Rng;
 
 fn spin_wheel() -> i32 {
     let mut rng = rand::thread_rng();
-    let wheel: Vec<i32> = [2500, 600, 700, 600, 650, 500, 700, 600, 550, 500, 600, 650, 700, 800, 500, 650, 500, 900,].to_vec();
+    let wheel = [2500, 600, 700, 600, 650, 500, 700, 600, 550, 500, 600, 650, 700, 800, 500, 650, 500, 900,];
         
     let index = rng.gen_range(0, wheel.len());
 
